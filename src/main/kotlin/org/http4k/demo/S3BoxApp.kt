6a821abeafda6ffc0810606442ef0628ee62bdb0
David Denton <denton.david@gmail.com>
2017-11-21 23:59:17 +0000

    added basic auth

--- src/main/kotlin/org/http4k/demo/S3BoxApp.kt
+++ src/main/kotlin/org/http4k/demo/S3BoxApp.kt
@@ -12,7 +12,10 @@ import org.http4k.core.Response
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Status.Companion.SEE_OTHER
+import org.http4k.core.then
 import org.http4k.core.with
+import org.http4k.demo.Settings.CREDENTIALS
+import org.http4k.filter.ServerFilters
 import org.http4k.lens.Header.Common.CONTENT_TYPE
 import org.http4k.lens.MultipartFormFile
 import org.http4k.lens.Path
@@ -72,12 +75,14 @@ object S3BoxApp {
     operator fun invoke(config: Configuration): RoutingHttpHandler {
         val s3 = S3.configured(config)
 
-        return routes(
-            "/{id}/delete" bind POST to Delete(s3),
-            "/{id}" bind GET to Get(s3),
-            "/" bind routes(
-                POST to Upload(s3),
-                GET to Index(s3)
+        return  ServerFilters.BasicAuth("http4k", config[CREDENTIALS]).then(
+            routes(
+                "/{id}/delete" bind POST to Delete(s3),
+                "/{id}" bind GET to Get(s3),
+                "/" bind routes(
+                    POST to Upload(s3),
+                    GET to Index(s3)
+                )
             )
         )
     }
--- src/main/kotlin/org/http4k/demo/Settings.kt
+++ src/main/kotlin/org/http4k/demo/Settings.kt
@@ -4,18 +4,23 @@ import io.github.konfigur8.ConfigurationTemplate
 import io.github.konfigur8.Property
 import org.http4k.aws.AwsCredentialScope
 import org.http4k.aws.AwsCredentials
+import org.http4k.core.Credentials
 import org.http4k.core.Uri
 
 object Settings {
+    val CREDENTIALS = Property("CREDENTIALS", String::toCredentials, Credentials::fromCredentials)
     val S3_CREDENTIALS = Property("S3_REGION", { AwsCredentialScope(it, "s3") }, AwsCredentialScope::region)
-    val AWS_CREDENTIALS = Property("AWS_CREDENTIALS", String::toCredentials, AwsCredentials::fromCredentials)
+    val AWS_CREDENTIALS = Property("AWS_CREDENTIALS", String::toAwsCredentials, AwsCredentials::fromCredentials)
     val AWS_BUCKET_URL = Property("AWS_BUCKET", { Uri.of("https://$it.s3.amazonaws.com")})
 
     val defaults = ConfigurationTemplate()
+        .requiring(CREDENTIALS)
         .requiring(S3_CREDENTIALS)
         .requiring(AWS_CREDENTIALS)
         .requiring(AWS_BUCKET_URL)
 }
 
 fun AwsCredentials.fromCredentials() = "$accessKey:$secretKey"
-fun String.toCredentials() = split(":").run { AwsCredentials(get(0), get(1)) }
+fun String.toAwsCredentials() = split(":").run { AwsCredentials(get(0), get(1)) }
+fun Credentials.fromCredentials() = "$user:$password"
+fun String.toCredentials() = split(":").run { Credentials(get(0), get(1)) }
